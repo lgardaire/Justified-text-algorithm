69db3327e403f17d86110aba2dbcbc50cb8563aa
Loic Gardaire <loic.gardaire@gmail.com>
2018-03-14 14:52:21 +0100

    penalty calculation

--- algorithm.py
+++ algorithm.py
@@ -3,8 +3,15 @@ def main():
     file = "examples/decl.txt"
     f = open(file, 'r', -1, "utf-8")
     lines = [i.strip(" \n") for i in f.readlines()]
-    print(lines)
     f.close()
+    words = []
+    [words.extend(line.split()) for line in lines]
+    print(words)
+
+
+def square_penalty(M, words):
+    len_words = sum(len(s) for s in words)
+    return (M - len(words) + 1 - len_words) ** 2
 
 
 if __name__ == '__main__':
