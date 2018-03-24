f8e5b1b46bfdaf71ec9f1507ef93ee2eb92a8e23
Loic Gardaire <loic.gardaire@gmail.com>
2018-03-24 16:19:03 +0100

    documentation

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,11 @@
-import time
-
-
 def justification(words, width):
+    """
+    Compute the justification on the text passed in parameter as an array
+    :param words: array of words forming the text to justify
+    :param width: maximum line width in the justified text
+    :return: array of justified text lines, penalty for the justification
+    """
+
     word_count = len(words)
     minimum = [0] + [10 ** 20] * word_count
     breaks = [0] * word_count
@@ -28,10 +32,25 @@ def justification(words, width):
 
 
 def line_length(i, j, words_length):
+    """
+    Calculate the length of a line of words
+    :param i: index of the first word of the line
+    :param j: index of the last word of the line
+    :param words_length: array containing word lengths
+    :return: length of the line
+    """
+
     return sum(words_length[i:j + 1]) + j - i
 
 
 def run_algorithm(text, M):
+    """
+    Split the text to justify in paragraphs and justify each of them
+    :param text: text to justify
+    :param M: maximum line width in the justified text
+    :return: justified text, penalty for this text
+    """
+
     paragraphs = text.split("\n")
     result = ""
     penalty = 0
@@ -44,6 +63,12 @@ def run_algorithm(text, M):
 
 
 def prepare_paragraph(filename):
+    """
+    Open the file to justify and read its content
+    :param filename: name of the file to open
+    :return: content of the file
+    """
+
     file = open(filename, 'r', encoding="utf-8")
     lines = file.readlines()
     file.close()
@@ -51,6 +76,13 @@ def prepare_paragraph(filename):
 
 
 def run_algorithm_to_optimize(filename, start, end):
+    """
+    Justify a text with different maximum lengths for a line to find the best one
+    :param filename: name of the file to open
+    :param start: minimum of the maximum length of the line
+    :param end: maximum of the maximum length of the line
+    """
+
     paragraph = prepare_paragraph(filename)
     results = {}
     for M in range(start, end + 1):
